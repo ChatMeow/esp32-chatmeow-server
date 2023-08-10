d619b2fcd65f17d4465d0d71200dece4070828cc
MeowKJ <ijink@icloud.com>
2023-08-10 12:08:43 +0800

    初始功能 chatgpt和vits

---
+++ meow_chatgpt/chat/chat.py
@@ -0,0 +1,100 @@
+from revChatGPT.V1 import Chatbot
+import logging
+
+from configparser import ConfigParser
+import logging
+import os
+
+
+def is_config_exists() -> bool:
+    return os.path.exists("./openai.ini")
+
+
+def generate_if_not_config_exists() -> bool:
+    if is_config_exists():
+        logging.info("使用已经存在的openai配置文件openai.ini")
+        return False
+    logging.info("openai配置文件不存在 生成配置文件openai.ini")
+    config = ConfigParser()
+    config.add_section("OpenAI")
+    config["OpenAI"]["access_token"] = "访问https://chat.openai.com/api/auth/session得到accesstoken *注意不是OpenAI API*"
+    # config["OpenAI"]["email"] = ""
+    # config["OpenAI"]["password"] = ""
+    config["OpenAI"]["conversation_id"] = ""
+    config["OpenAI"]["prompt"] = ""
+    with open("./openai.ini", "w") as configfile:
+        config.write(configfile)
+    return True
+
+
+class MeowConfig:
+    def __init__(self) -> None:
+        self.config = ConfigParser()
+        self.config.read("./openai.ini", encoding="UTF-8")
+        self.conversation_id = self.config["OpenAI"]["conversation_id"]
+
+    def get_openai_config(self) -> dict[str:str]:
+        openai_config = self.config["OpenAI"]
+        openai_access_token = openai_config["access_token"]
+        openai_email = openai_config["email"]
+        openai_password = openai_config["password"]
+        if openai_access_token:
+            logging.info("use openai access_token")
+            return {"access_token": openai_access_token}
+        if openai_email and openai_password:
+            logging.info("use openai eamil {}".format(openai_email))
+            return {"email": openai_email, "password": openai_password}
+        else:
+            raise Exception("读取不到openai配置 请检查配置文件")
+
+    def get_promot(self) -> str:
+        return self.config["OpenAI"]["prompt"]
+
+    def get_conversation_id(self) -> str:
+        return self.conversation_id
+
+    def set_conversation_id(self, new_conversation_id: str):
+        self.conversation_id = new_conversation_id
+        self.config["OpenAI"]["conversation_id"] = new_conversation_id
+        with open("./openai.ini", "w") as configfile:
+            self.config.write(configfile)
+
+
+class ChatModule:
+    prev_text = ""
+
+    def __init__(self) -> None:
+        if generate_if_not_config_exists():
+            logging.info("openai配置文件已经生成 请修改配置文件openai.ini并重新启动程序")
+            exit(0)
+        conf = MeowConfig()
+
+        # 获取openai配置
+        openai_config = conf.get_openai_config()
+        conversation_id = conf.get_conversation_id()
+
+        if not conf.conversation_id:
+            logging.info("对话id不存在 创建第一次会话")
+            prompt = conf.get_promot()
+            self.chatbot = Chatbot(config=openai_config)
+
+            logging.info("YOU: {}".format(prompt))
+            conversation_id, msg = self.get_conversation_id(prompt)
+            logging.info("BOT: {}".format(msg))
+            conf.set_conversation_id(conversation_id)
+        else:
+            logging.info("对话id存在 使用对话id -> {}".format(conversation_id))
+            self.chatbot = Chatbot(config=openai_config, conversation_id=conversation_id)
+
+    def get_conversation_id(self, prompt: str) -> (str, str):
+        response = ""
+        for data in self.chatbot.ask(prompt):
+            conversation_id = data["conversation_id"]
+            response = data["message"]
+        return conversation_id, response
+
+    def chat(self, prompt: str) -> str:
+        response = ""
+        for data in self.chatbot.ask(prompt):
+            response = data["message"]
+        return response
---
+++ meow_chatgpt/vits/vits_tts.py
@@ -0,0 +1,124 @@
+# coding=utf-8
+import os
+import traceback
+import logging
+from configparser import ConfigParser
+
+load_model = True
+
+def save_vits_wav(text, uuid):
+    if load_model:
+        import numpy as np
+        from scipy.io.wavfile import write
+        vits = Vits()
+        status, voice = vits.get_vits_voice_tuple(text)
+        if not status:
+            logging.error("vits音频文件生成失败，请检查配置文件参数")
+            return False
+        try:
+            scaled = np.int16(voice[1] / np.max(np.abs(voice[1])) * 32767)
+            file_path = os.path.join(os.getcwd(), 'voice_tmp', 'v' + uuid + '.wav')
+            write(file_path, 22050, scaled)
+            logging.info("成功生成WAV文件[v_{}.wav]".format(uuid))
+            return True
+        except Exception:
+            traceback.print_exc()
+            return False
+    return False
+
+
+def get_text(text, hps):
+    import meow_chatgpt.vits.commons as commons
+    from torch import LongTensor
+    from meow_chatgpt.vits.text import text_to_sequence
+    text_norm, clean_text = text_to_sequence(text, hps.symbols, hps.data.text_cleaners)
+    if hps.data.add_blank:
+        text_norm = commons.intersperse(text_norm, 0)
+    text_norm = LongTensor(text_norm)
+    return text_norm, clean_text
+
+
+def change_lang(language):
+    if language == 0:
+        return 0.6, 0.668, 1.2
+    else:
+        return 0.6, 0.668, 1.1
+
+
+class Vits:
+    def __init__(self):
+        self.config = ConfigParser()
+        self.config.read("./config.ini", encoding="UTF-8")
+        vits_config = self.config['vits']
+
+        model_folder = vits_config['model_folder']
+        self.model_path = os.path.join(os.getcwd(), model_folder, vits_config['model_name'])
+        self.config_path = os.path.join(os.getcwd(), model_folder, vits_config['config_name'])
+        if not os.path.exists(self.model_path) or not os.path.exists(self.config_path):
+            logging.error("{}文件夹中没有模型[{}]或配置文件[{}]".format(model_folder, self.model_path, self.config_path))
+            exit(1)
+
+        self.speakers = None
+        self.model = None
+        self.optimizer = None
+        self.learning_rate = None
+        self.epochs = None
+        self.hps_ms = None
+        self.net_g_ms = None
+        self.device = None
+        self.device_text = vits_config['device']
+        self.ns = float(vits_config['ns'])
+        self.nsw = float(vits_config['nsw'])
+        self.ls = float(vits_config['ls'])
+        self.lang = vits_config['lang']
+        self.speak_id = int(vits_config['speak_id'])
+
+    def vits(self, text, language, speaker_id, noise_scale, noise_scale_w, length_scale):
+        from torch import no_grad, LongTensor
+        text = text.replace('\n', ' ').replace('\r', '').replace(" ", "")
+        if language == 'zh':
+            text = f"[ZH]{text}[ZH]"
+        elif language == 'ja':
+            text = f"[JA]{text}[JA]"
+        elif language == 'mix':
+            text = f"{text}"
+        else:
+            logging.error('vits：错误的语言，请检查配置文件')
+            return False, None
+        stn_tst, clean_text = get_text(text, self.hps_ms)
+        with no_grad():
+            x_tst = stn_tst.unsqueeze(0).to(self.device)
+            x_tst_lengths = LongTensor([stn_tst.size(0)]).to(self.device)
+            speaker_id = LongTensor([speaker_id]).to(self.device)
+            audio = \
+                self.net_g_ms.infer(x_tst, x_tst_lengths, sid=speaker_id, noise_scale=noise_scale,
+                                    noise_scale_w=noise_scale_w,
+                                    length_scale=length_scale)[0][0, 0].data.cpu().float().numpy()
+
+        return True, (22050, audio)
+
+    def search_speaker(self, search_value):
+        for s in self.speakers:
+            if search_value == s:
+                return s
+        for s in self.speakers:
+            if search_value in s:
+                return s
+
+    def get_vits_voice_tuple(self, text_to_voice):
+        from meow_chatgpt.vits.models import SynthesizerTrn
+        import meow_chatgpt.vits.utils as utils
+        import torch
+        self.device = torch.device(self.device_text)
+        self.hps_ms = utils.get_hparams_from_file(self.config_path)
+        self.net_g_ms = SynthesizerTrn(
+            len(self.hps_ms.symbols),
+            self.hps_ms.data.filter_length // 2 + 1,
+            self.hps_ms.train.segment_size // self.hps_ms.data.hop_length,
+            n_speakers=self.hps_ms.data.n_speakers,
+            **self.hps_ms.model)
+        _ = self.net_g_ms.eval().to(self.device)
+        self.speakers = self.hps_ms.speakers
+        self.model, self.optimizer, self.learning_rate, self.epochs = utils.load_checkpoint(self.model_path, self.net_g_ms,
+                                                                                            None)
+        return self.vits(text_to_voice, self.lang, self.speak_id, self.ns, self.nsw, self.ls)
